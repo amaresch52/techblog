91defd972f4d4525fc3ca9d43f8de0dac74adca5
Tom Hanks <amaresch@gmail.com>
2021-08-11 18:36:09 -0500

    changed db, login, logout and added js files

---
+++ public/js/add-post.js
@@ -0,0 +1,27 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value;
+    const content = document.querySelector('#content').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document
+    .querySelector('#createPost')
+    .addEventListener('submit', newFormHandler);
